6c7aefb17036fb994b5f7808d6bd0239dca66607
unknown <sezeranochrisostom123@gmail.com>
2022-01-27 08:55:48 +0200

    Fixing connection in single folder for it to be used globaly

--- server/main.js
+++ server/main.js
@@ -1,16 +1,6 @@
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-/* Database configuration */
-const firebaseConfig = {
-    apiKey: "AIzaSyDyhJPX5hGXmkVCjWiDYDgxW8Ongh0YZqU",
-    authDomain: "capstone-d17ab.firebaseapp.com",
-    projectId: "capstone-d17ab",
-    storageBucket: "capstone-d17ab.appspot.com",
-    messagingSenderId: "658315154433",
-    appId: "1:658315154433:web:ee6855874b6ed722da1c00",
-    databaseURL:"https://capstone-d17ab-default-rtdb.europe-west1.firebasedatabase.app"
-};
 
 /* Initialize Database  */
 const app = firebase.initializeApp(firebaseConfig);
